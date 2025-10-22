0fe619ddcb613f7d0dace076ca3e16cd39faabad
jett <jett@ztgame.com>
2025-10-22 17:16:17 +0800

    feat: 添加langchain

--- lib/features/chat/view.dart
+++ lib/features/chat/view.dart
@@ -1,12 +1,37 @@
 import 'package:adaptive_theme/adaptive_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:langchain/langchain.dart';
+import 'package:langchain_openai/langchain_openai.dart';
 
+import '../../core/log/log.dart';
 import 'bloc.dart';
 import 'event.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   const ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  @override
+  void initState() {
+    super.initState();
+    _invoke();
+  }
+
+  Future<void> _invoke() async {
+    final model = ChatOpenAI(
+      apiKey: 'sk-',
+      baseUrl: 'https://api.moonshot.cn/v1',
+      defaultOptions:  const ChatOpenAIOptions(model: 'kimi-k2-0711-preview')
+    );
+    final prompt = PromptValue.string('你是谁？');
+    final result = await model.invoke(prompt);
+    logger.i(result);
+  }
 
   @override
   Widget build(BuildContext context) {
